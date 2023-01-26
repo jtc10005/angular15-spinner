159718f2e1f1e7b6c9aab14b41dd008b1dd9ac56
jtc10005 <jtc10005@gmail.com>
2023-01-26 12:54:29 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular15-spinner
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-zsnfkb)
---
+++ src/app/progress-spinner-overview-example.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic progress-spinner
+ */
+@Component({
+  selector: 'progress-spinner-overview-example',
+  templateUrl: 'progress-spinner-overview-example.html',
+})
+export class ProgressSpinnerOverviewExample {}
+
+
+/**  Copyright 2023 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
